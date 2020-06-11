19fc4ffc6f1387068b4113993c7b598282012362
andriyboyko <43106880+andriyboyko@users.noreply.github.com>
2020-06-11 04:30:42 +0300

    lab4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ROUTING } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ShowPostComponent } from './show-post/show-post.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { CommonService } from './service/common.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+  	RootComponent,
+    LoginComponent,
+    HomeComponent,
+    ShowPostComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ROUTING,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [CommonService],
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
